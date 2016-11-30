29a12268db989c7d3a2490777c705dcf1d71bc30
earthquaker <pe_lias@msn.com>
2016-11-30 11:27:26 +0100

    formated correcly

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
-import { Row, Col, Menu, Icon, Card } from 'antd';
+import {Row, Col, Menu, Icon, Card} from 'antd';
 
 class App extends Component {
 
@@ -8,11 +8,11 @@ class App extends Component {
     return (
       <div>
         <Menu mode="horizontal" theme="light">
-          <Menu.Item key="engage" style={{fontSize:'17px'}}>
+          <Menu.Item key="engage" style={{fontSize: '17px'}}>
             Engage Engine
           </Menu.Item>
-          <Menu.Item key="logout" style={{float:'right'}}>
-            <Icon type="logout" />Logout
+          <Menu.Item key="logout" style={{float: 'right'}}>
+            <Icon type="logout"/>Logout
           </Menu.Item>
         </Menu>
 
@@ -20,21 +20,21 @@ class App extends Component {
           <Col span={4}>
             <Menu>
               <Menu.Item key="dashboard">
-                <Icon type="appstore-o" />Dashboard
+                <Icon type="appstore-o"/>Dashboard
               </Menu.Item>
               <Menu.Item key="Engage">
-                <Icon type="heart-o" />Engage
+                <Icon type="heart-o"/>Engage
               </Menu.Item>
               <Menu.Item key="statistics">
-                <Icon type="bar-chart" />Statistics
+                <Icon type="bar-chart"/>Statistics
               </Menu.Item>
               <Menu.Item key="installation">
-                <Icon type="info" />Installation
+                <Icon type="info"/>Installation
               </Menu.Item>
             </Menu>
           </Col>
           <Col span={20}>
-            <div style={{ padding: '30px' }}>
+            <div style={{padding: '30px'}}>
               <Row>
                 <Col span="8">
                   <Card title="Card title">Card content</Card>
