dad4967082a0fc66c3a2c76f71318dc140c61669
Elias Hansen <pe_lias@msn.com>
2016-12-08 01:53:44 +0100

    updated website settings with navigation type

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
-import {Row, Col, Tabs} from 'antd';
+import {Row, Col, Tabs, Radio, Card, Tooltip} from 'antd';
 const TabPane = Tabs.TabPane;
+const RadioGroup = Radio.Group;
 
 import AppHeader from './../AppHeader';
 import AppMenu from './../AppMenu';
 
 class Settings extends Component {
   render() {
+    const radioStyle = {
+      display: 'block',
+      height: '30px',
+      lineHeight: '30px',
+    };
+
+    const toolT = (
+      <Tooltip placement="top" title={"Select type of navigation your website uses. (Normal websites use Default page load, Single Page Applications uses Hashchange or Popstate)."}>
+        <a href="#">?</a>
+      </Tooltip>
+    );
+
     return (
       <div>
         <AppHeader />
@@ -17,8 +30,16 @@ class Settings extends Component {
           </Col>
           <Col span={20}>
             <Tabs defaultActiveKey="1" style={{padding:30}}>
-              <TabPane tab="Account Settings" key="1">Account Settings</TabPane>
-              <TabPane tab="Website Settings" key="2">Website Settings</TabPane>
+              <TabPane tab="Website Settings" key="1">
+                <Card title="Navigation Type" extra={toolT} style={{ width: 300 }}>
+                  <RadioGroup>
+                    <Radio style={radioStyle} value={1}>Default page load</Radio>
+                    <Radio style={radioStyle} value={2}>Hashchange</Radio>
+                    <Radio style={radioStyle} value={3}>Popstate</Radio>
+                  </RadioGroup>
+                </Card>
+              </TabPane>
+              <TabPane tab="Account Settings" key="2">Account Settings</TabPane>
             </Tabs>
           </Col>
         </Row>
